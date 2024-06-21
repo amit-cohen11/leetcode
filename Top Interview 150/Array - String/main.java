2a5559f2e68b605ad3c1f86cea9bee8c4e00e932
amit cohen <cohen415@walla.com>
2024-06-21 10:55:47 +0300

    removed redundant code

--- Top Interview 150/Array - String/main.java
+++ Top Interview 150/Array - String/main.java
@@ -13,9 +13,6 @@ public class main {
     int i = nums1.length - 1;
 
     while (n > 0) {
-      if (m == 0)
-        nums1[i] = nums2[n - 1];
-
       if (m != 0 && nums1[m - 1] > nums2[n - 1]) {
         nums1[i] = nums1[m - 1];
         m--;
