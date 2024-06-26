9726e409719d8bb33cfc50a286d423b7bb99be25
amit cohen <cohen415@walla.com>
2024-06-26 22:08:11 +0300

    finished java

--- Top Interview 150/Array - String/Q-26/main.java
+++ Top Interview 150/Array - String/Q-26/main.java
@@ -1,13 +1,23 @@
 import java.util.*;
 
 public class main {
-  public static void main(String[] args) {
-    int[] nums = { 0, 1, 2, 2, 3, 0, 4, 2, 2, 4 };
-    int val = 2;
-    removeElement(nums, val);
+  public static void main (String[] args) {
+    int[] nums = { 0, 0, 0, 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
+    removeDuplicates(nums);
   }
 
-  public static void removeElement(int[] nums, int val) {
-    
+  public static int removeDuplicates(int[] nums) {
+    int curPosition = 0;
+    int nextPosition = 0;
+    while (nextPosition < nums.length - 1){
+      nextPosition++;
+      if(nums[curPosition] != nums[nextPosition]){
+        curPosition++;
+        nums[curPosition] = nums[nextPosition];
+      }
+    }
+    System.out.println(Arrays.toString(nums));
+    System.out.println(++curPosition);
+    return ++curPosition;
   }
 }
