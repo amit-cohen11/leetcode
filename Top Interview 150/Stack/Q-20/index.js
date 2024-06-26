80d112d565914751ec2450ebd1af7d3966dc858c
amit cohen <cohen415@walla.com>
2024-06-26 23:09:53 +0300

    finished js

---
+++ Top Interview 150/Stack/Q-20/index.js
@@ -0,0 +1,44 @@
+const isValid = (str) => {
+  const brackets = { "(": ")", "[": "]", "{": "}" };
+  let bracketsStack = [];
+  if (str === "") return true;
+
+  bracketsStack.push(str[0]);
+  let i = 1;
+  while (i < str.length) {
+    isOpenBracket = Object.keys(brackets).find((br) => br === str[i]);
+    if (isOpenBracket) {
+      bracketsStack.push(str[i]);
+    } else if (str[i] === brackets[bracketsStack[bracketsStack.length - 1]]) {
+      bracketsStack.pop();
+    } else {
+      return false;
+    }
+    i++;
+  }
+  return bracketsStack.length === 0;
+};
+
+const Stack = require("stackjs");
+const isValid2 = (str) => {
+  const brackets = { "(": ")", "[": "]", "{": "}" };
+  let bracketsStack = new Stack();
+  if (str === "") return true;
+
+  bracketsStack.push(str[0]);
+  let i = 1;
+  while (i < str.length) {
+    isOpenBracket = Object.keys(brackets).find((br) => br === str[i]);
+    if (isOpenBracket) {
+      bracketsStack.push(str[i]);
+    } else if (str[i] === brackets[bracketsStack.peek()]) {
+      bracketsStack.pop();
+    } else {
+      return false;
+    }
+    i++;
+  }
+  return bracketsStack.isEmpty();
+};
+
+console.log(isValid("()"));
---
+++ Top Interview 150/Stack/Q-20/main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class main {
+  public static void main (String[] args) {
+    String str = "()[]{}";
+    isValid(str);
+  }
+  
+  public static void isValid(String str) {
+    System.out.println("Asa");
+  }
+}
+
