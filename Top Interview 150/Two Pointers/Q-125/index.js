bcba44a37e2395d4a0a1d4c456faddcc4cedf096
amit cohen <cohen415@walla.com>
2024-06-28 13:59:06 +0300

    finished

---
+++ Top Interview 150/Two Pointers/Q-125/index.js
@@ -0,0 +1,15 @@
+const isPalindrome = (str) => {
+  const alphanumericStr = str.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+  let left = 0;
+  let right = alphanumericStr.length - 1;
+  while (left < right) {
+    if (alphanumericStr[left] != alphanumericStr[right]) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+  return true;
+};
+
+console.log(isPalindrome("A man, a plan, a canal: Panama"));
---
+++ Top Interview 150/Two Pointers/Q-125/main.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class main {
+  public static void main(String[] args) {
+    String str = "A man, a plan, a canal: Panama";
+    System.out.println(isPalindrome(str));
+  }
+
+  public static boolean isPalindrome(String str) {
+    String alphanumericStr = str.toLowerCase().replaceAll("[^a-zA-Z0-9]", "");
+    int left = 0, right = alphanumericStr.length() - 1;
+    while (left < right) {
+      if (alphanumericStr.charAt(left) != alphanumericStr.charAt(right))
+        return false;
+      left++;
+      right--;
+    }
+    return true;
+  }
+}
